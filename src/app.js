b8b5fe5e70942431f7cd24f3560482388405243a
Martin Beltramo <martin_beltramo@hotmail.com>
2024-09-05 19:03:40 -0300

    agrego handlebars + productos

--- src/app.js
+++ src/app.js
@@ -2,17 +2,30 @@
 import express from 'express';
 import productsRouter from './routes/products.router.js';
 import cartRouter from './routes/cart.router.js';
+import viewsRouter from './routes/views.router.js';
+import { engine } from 'express-handlebars';
+import path from 'path';
+import __dirname from './utils.js';
+
 
 // App Express
 const app = express();
 
+// Set up Handlebars
+app.engine('handlebars', engine());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
 // Middlewares
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Rutas
+// Routes
+app.use('/', viewsRouter);
 app.use('/api', productsRouter);
 app.use('/api', cartRouter);
 
-//Server Definition
-const PORT = process.env.PORT||8080;
-const server = app.listen(PORT,()=>console.log(`Listening on ${PORT}`));
+
+// Server Definition
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
